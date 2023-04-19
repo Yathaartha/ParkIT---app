a3cf63e323942a97d6930f30101ec7bb3320d4f9
Yathaartha Maharjan <maharjanyathaartha017@gmail.com>
2023-04-19 13:59:26 +0545

    Feat: add park button

---
+++ src/components/modules/home/ParkButton.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Button} from 'react-native';
+import styled from 'styled-components';
+import {dangerColors, primaryColors} from '../../../constants/colors';
+
+const ButtonWrap = styled.View`
+  padding: 20px;
+`;
+
+const ParkBtn = styled.Button`
+  border-radius: 20px;
+  padding: 30px 30px;
+`;
+
+const ParkButton = ({available}) => {
+  return (
+    <ButtonWrap>
+      <ParkBtn
+        title="Park"
+        color={available === 0 ? dangerColors.main : primaryColors.main}
+      />
+    </ButtonWrap>
+  );
+};
+
+export default ParkButton;
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import Header from '../components/header/Header';
-import DonutChart from '../components/donut-chart/DonutChart';
+import Availability from '../components/modules/home/Availability';
+import ParkButton from '../components/modules/home/ParkButton';
 
 const Home = () => {
+  const parkingAvailability = {
+    available: 2,
+    total: 30,
+  };
+
   return (
     <View>
       <Header />
-      <Text>Home Page</Text>
-      <DonutChart
-        numerator={20}
-        denominator={30}
-        radius={80}
-        strokeWidth={15}
+      <Availability
+        available={parkingAvailability.available}
+        total={parkingAvailability.total}
       />
+      <ParkButton available={parkingAvailability.available} />
     </View>
   );
 };
